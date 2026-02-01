41445ecc3ec799f39ebff8e26214e2a2929aaeee
songdm <xgyb12458@163.com>
2026-02-01 13:50:02 +0800

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: "resume",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
